c13696ab581143d8e29cc3d01742e44888d6d3b9
abhishekchand819 <abhishekchand055@gmail.com>
2020-07-24 17:37:38 +0530

    Screen 2 OnBoarding

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import {NativeRouter, Route} from 'react-router-native';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 import Home from './components/Home';
-import Screen1 from './components/OnBoarding/Screen1';
+import Screen2 from './components/OnBoarding/Screen2';
 
 export default function App() {
   changeNavigationBarColor('#1B0536');
   return (
     <NativeRouter>
       {/* <Route path="/" component={Home} /> */}
-      <Route path="/" component={Screen1} />
+      <Route path="/" component={Screen2} />
     </NativeRouter>
   );
 }
