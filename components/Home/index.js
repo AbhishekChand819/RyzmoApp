26228ca5915909673ed7e4048df36a9179072bc4
abhishekchand819 <abhishekchand055@gmail.com>
2020-07-25 09:39:24 +0530

    Home Component

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,13 +1,81 @@
 import React from 'react';
-import {View, StatusBar} from 'react-native';
+import {View, StatusBar, Text, ScrollView} from 'react-native';
 import {styles} from './styles';
 import Navbar from '../Navbar';
+import TrackLabel from '../shared/TrackLabel';
 
 function Home() {
   return (
     <React.Fragment>
       <StatusBar backgroundColor="#1B0536" />
-      <View style={styles.wrapper}></View>
+      <ScrollView
+        style={styles.wrapper}
+        overScrollMode="never"
+        showsHorizontalScrollIndicator={false}>
+        <View style={{paddingTop: 10}}>
+          <Text style={styles.labelText}>Recommended For You</Text>
+          <ScrollView
+            horizontal={true}
+            overScrollMode="never"
+            style={styles.container}
+            showsHorizontalScrollIndicator={false}>
+            <TrackLabel
+              text="Global Top 50"
+              image={require('../../assets/album1.jpg')}
+              gradient={['rgba(238, 0, 143, 0)', '#D708F9']}></TrackLabel>
+            <TrackLabel
+              text="Bollywood Mashup"
+              image={require('../../assets/album2.jpg')}
+              gradient={['rgba(255, 255, 255, 0)', '#00FFA3']}></TrackLabel>
+            <TrackLabel
+              text="Happy Wednesday"
+              image={require('../../assets/album3.jpg')}
+              gradient={['rgba(238, 255, 255, 0)', '#0137C7']}></TrackLabel>
+          </ScrollView>
+        </View>
+        <View style={{paddingTop: 40}}>
+          <Text style={styles.labelText}>Popular and Trending</Text>
+          <ScrollView
+            horizontal={true}
+            overScrollMode="never"
+            style={styles.container}
+            showsHorizontalScrollIndicator={false}>
+            <TrackLabel
+              text="Global Top 50"
+              image={require('../../assets/album1.jpg')}
+              gradient={['rgba(238, 0, 143, 0)', '#D708F9']}></TrackLabel>
+            <TrackLabel
+              text="Bollywood Mashup"
+              image={require('../../assets/album2.jpg')}
+              gradient={['rgba(255, 255, 255, 0)', '#00FFA3']}></TrackLabel>
+            <TrackLabel
+              text="Happy Wednesday"
+              image={require('../../assets/album3.jpg')}
+              gradient={['rgba(238, 255, 255, 0)', '#0137C7']}></TrackLabel>
+          </ScrollView>
+        </View>
+        <View style={{paddingTop: 40, paddingBottom: 20}}>
+          <Text style={styles.labelText}>Hindi New Releases</Text>
+          <ScrollView
+            horizontal={true}
+            overScrollMode="never"
+            style={styles.container}
+            showsHorizontalScrollIndicator={false}>
+            <TrackLabel
+              text="Global Top 50"
+              image={require('../../assets/album1.jpg')}
+              gradient={['rgba(238, 0, 143, 0)', '#D708F9']}></TrackLabel>
+            <TrackLabel
+              text="Bollywood Mashup"
+              image={require('../../assets/album2.jpg')}
+              gradient={['rgba(255, 255, 255, 0)', '#00FFA3']}></TrackLabel>
+            <TrackLabel
+              text="Happy Wednesday"
+              image={require('../../assets/album3.jpg')}
+              gradient={['rgba(238, 255, 255, 0)', '#0137C7']}></TrackLabel>
+          </ScrollView>
+        </View>
+      </ScrollView>
       <Navbar></Navbar>
     </React.Fragment>
   );
--- components/Home/styles.js
+++ components/Home/styles.js
@@ -3,9 +3,21 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   wrapper: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
     backgroundColor: '#1B0536',
-    height: '92.2%',
+    paddingLeft: 20,
+    paddingBottom: 10,
+  },
+  container: {
+    flexDirection: 'row',
+  },
+  labelText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 24,
+    lineHeight: 29,
+    color: '#FFFFFF',
+    alignItems: 'center',
+    marginBottom: 2,
+    paddingTop: 10,
+    paddingBottom: 10,
   },
 });
